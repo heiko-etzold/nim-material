04d02060fa6af37f7929835a89adeb0ba6731f2c
Heiko Etzold <heiko.etzold@uni-potsdam.de>
2023-04-18 14:22:52 +0200

    version 1.1

--- Makefile.R
+++ Makefile.R
@@ -7,8 +7,8 @@ shortTitle <- "Nim-Handreichung"
 description <- "Anleitungen und Hinweise zur Nutzung der Nim-App"
 
 versionText <- "Version"
-guideVersion <- "1.0"
-appVersion <- "1.0"
+guideVersion <- "1.1"
+appVersion <- "1.1"
 optimizedTextA <- "; angepasst an Version "
 optimizedTextB <- " der Nim-App"
 
